369052c98d80e251bfacc3997348936a9af17e16
negri234279 <rafalejo@live.com>
2023-04-14 00:00:19 +0200

    👔 Add create workout

---
+++ src/Contexts/Workouts/controllers/create.controller.ts
@@ -0,0 +1,26 @@
+import { UserRole } from 'src/Contexts/Users/userRole'
+import { ReqPayload } from 'src/Core/infrastructure/@types/express'
+
+import { Body, Controller, ForbiddenException, Post, Req } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+
+import { CreateWorkoutDto } from '../dtos/createWorkout.dto'
+import { WorkoutCreateService } from '../services/create.service'
+
+@ApiTags('Workouts')
+@Controller('workouts')
+export class WorkoutCreateController {
+    constructor(private readonly workoutCreateService: WorkoutCreateService) {}
+
+    @Post()
+    async execute(
+        @Req() req: ReqPayload,
+        @Body() body: CreateWorkoutDto,
+    ): Promise<void> {
+        if (!req.user.role.includes(UserRole.ATHLETE)) {
+            throw new ForbiddenException()
+        }
+
+        this.workoutCreateService.execute(req.user, body)
+    }
+}
---
+++ src/Contexts/Workouts/dtos/createWorkout.dto.ts
@@ -0,0 +1,63 @@
+import {
+    IsBoolean,
+    IsDateString,
+    IsNotEmpty,
+    IsNumber,
+    IsOptional,
+    IsString,
+    IsUUID,
+} from 'class-validator'
+
+import { ApiProperty } from '@nestjs/swagger'
+
+export class CreateWorkoutDto {
+    @ApiProperty({ type: 'string', format: 'uuid' })
+    @IsUUID()
+    @IsNotEmpty()
+    id: string
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    name: string
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    sets: number
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    reps: number
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    weight: number
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsDateString()
+    date: Date
+
+    @ApiProperty()
+    @IsOptional()
+    @IsBoolean()
+    isCompleted?: boolean
+
+    @ApiProperty()
+    @IsOptional()
+    @IsBoolean()
+    isSuccessful?: boolean
+
+    @ApiProperty({ type: 'string', format: 'uuid' })
+    @IsUUID()
+    @IsNotEmpty()
+    athleteId: string
+
+    @ApiProperty({ type: 'string', format: 'uuid' })
+    @IsUUID()
+    @IsOptional()
+    coachId?: string
+}
