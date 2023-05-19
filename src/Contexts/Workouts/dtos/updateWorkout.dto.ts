40bf12ab4e1e779b2556849f705abeb64247005c
negri234279 <rafalejo@live.com>
2023-05-19 22:44:30 +0200

    🐛 Fix workout model properties

--- src/Contexts/Workouts/dtos/updateWorkout.dto.ts
+++ src/Contexts/Workouts/dtos/updateWorkout.dto.ts
@@ -1,5 +1,19 @@
 import { OmitType, PartialType } from '@nestjs/mapped-types'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsBoolean, IsOptional } from 'class-validator'
 
 import { CreateWorkoutDto } from './createWorkout.dto'
 
-export class UpdateworkoutDto extends PartialType(OmitType(CreateWorkoutDto, ['id'])) {}
+export class UpdateworkoutDto extends PartialType(
+    OmitType(CreateWorkoutDto, ['id', 'isCompleted', 'isSuccessful']),
+) {
+    @ApiProperty()
+    @IsOptional()
+    @IsBoolean()
+    isCompleted: boolean
+
+    @ApiProperty()
+    @IsOptional()
+    @IsBoolean()
+    isSuccessful: boolean
+}
