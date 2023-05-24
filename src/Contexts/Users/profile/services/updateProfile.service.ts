20c020dce17b119ece87b3e66423123939d7fdbf
negri234279 <rafalejo@live.com>
2023-05-25 01:03:50 +0200

    Update UpdateUserProfileService with validations email and username

--- src/Contexts/Users/profile/services/updateProfile.service.ts
+++ src/Contexts/Users/profile/services/updateProfile.service.ts
@@ -14,6 +14,23 @@ export class UpdateUserProfileService {
             throw new ConflictException()
         }
 
+        if (profileDto.username) {
+            const existingUsername = await this.usersRepository.findOneBy(
+                'username',
+                profileDto.username,
+            )
+            if (existingUsername && user.username !== profileDto.username) {
+                throw new ConflictException('Username already exists')
+            }
+        }
+
+        if (profileDto.email) {
+            const existingEmail = await this.usersRepository.findOneBy('email', profileDto.email)
+            if (existingEmail && user.email !== profileDto.email) {
+                throw new ConflictException('Email already exists')
+            }
+        }
+
         const updateUser = new UserModel({ ...user, ...profileDto })
 
         await this.usersRepository.update(updateUser)
