66fa477b1225ed3840e4324d867042f93a157594
negri234279 <rafalejo@live.com>
2023-05-30 20:48:43 +0200

    🌱 Update seeder workout

--- src/Contexts/Comments/database/comment.seeder.ts
+++ src/Contexts/Comments/database/comment.seeder.ts
@@ -25,7 +25,7 @@ export class CommentSeeder implements Seeder {
         const athleteId = athlete.id
         const coachId = coach.id
 
-        const workouts = await this.workoutsRepository.findByAthelte(athleteId)
+        const workouts = await this.workoutsRepository.find(athleteId)
 
         for (const { id: workoutId } of workouts) {
             const numComments = faker.datatype.number({ min: 1, max: 3 })
