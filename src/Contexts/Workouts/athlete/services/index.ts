53857ac9e0dd0fc24a0e6dc4e5efd8260b559f66
negri234279 <rafalejo@live.com>
2023-05-30 20:51:16 +0200

    ⚰️ Remove service for sort and filter workouts

--- src/Contexts/Workouts/athlete/services/index.ts
+++ src/Contexts/Workouts/athlete/services/index.ts
@@ -1,10 +1,8 @@
 import { WorkoutCreateService } from './create.service'
 import { WorkoutDeleteService } from './delete.service'
-import { FilterWorkoutsService } from './filterWorkouts.service'
 import { WorkoutFindByAthleteService } from './findByAthlete.service'
 import { WorkoutFindByCoachAthleteService } from './findByCoachAthlete.service'
 import { WorkoutFindOneByAthleteService } from './findOneByAthlete.service'
-import { SortWorkoutsService } from './sortWorkout.service'
 import { WorkoutUpdateService } from './update.service'
 
 export const WorkoutAthleteServices = [
@@ -14,6 +12,4 @@ export const WorkoutAthleteServices = [
     WorkoutCreateService,
     WorkoutUpdateService,
     WorkoutDeleteService,
-    FilterWorkoutsService,
-    SortWorkoutsService,
 ]
