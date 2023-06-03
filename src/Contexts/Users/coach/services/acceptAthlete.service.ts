e067f98474f8745dbb325e65bd50ac5db70a9c7b
negri234279 <rafalejo@live.com>
2023-06-03 19:48:04 +0200

    Fix: UserCoachAcceptAthleteService the coach wasn't saved in athlete

--- src/Contexts/Users/coach/services/acceptAthlete.service.ts
+++ src/Contexts/Users/coach/services/acceptAthlete.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common'
 
 import { UsersRepository } from '../../shared/database/users.repository'
 
@@ -7,18 +7,28 @@ export class UserCoachAcceptAthleteService {
     constructor(private readonly usersRepository: UsersRepository) {}
 
     async execute(idAthlete: string, idCoach: string): Promise<void> {
-        const athlete = await this.usersRepository.findOne(idAthlete)
-        if (!athlete || !athlete.isAthlete()) {
-            throw new NotFoundException()
-        }
-
         const coach = await this.usersRepository.findOne(idCoach)
-        if (!coach || !coach.isCoach()) {
+        if (!coach) {
             throw new NotFoundException()
         }
 
+        if (!coach.isCoach()) {
+            throw new ConflictException()
+        }
+
+        const athlete = await this.usersRepository.findOne(idAthlete)
+        if (!athlete) {
+            throw new NotFoundException()
+        }
+
+        if (!athlete.isAthlete()) {
+            throw new ConflictException()
+        }
+
+        athlete.acceptCoach(idCoach)
         coach.acceptAthlete(idAthlete)
 
+        await this.usersRepository.update(athlete)
         await this.usersRepository.update(coach)
     }
 }
