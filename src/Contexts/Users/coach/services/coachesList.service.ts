93592b4c8b2f87cee1f0bc368a448dd02973e2f4
negri234279 <rafalejo@live.com>
2023-06-04 20:45:11 +0200

    👔 Feat: coaches list with search

--- src/Contexts/Users/coach/services/coachesList.service.ts
+++ src/Contexts/Users/coach/services/coachesList.service.ts
@@ -6,6 +6,7 @@ import { PaginationDto } from 'src/Core/infrastructure/dtos/pagination.dto'
 import { UsersRepository } from '../../shared/database/users.repository'
 import { UserModel } from '../../shared/user.model'
 import { UserRole } from '../../shared/userRole'
+import { SearchWithPaginationDto } from '../../shared/dtos/searchWithPagination.dto'
 
 @Injectable()
 export class UserCoachesListService {
@@ -14,7 +15,10 @@ export class UserCoachesListService {
         private readonly paginationService: PaginationService,
     ) {}
 
-    async execute(id: string, pagination: PaginationDto): Promise<PaginationRes<UserModel>> {
+    async execute(
+        id: string,
+        { search, ...pagination }: SearchWithPaginationDto,
+    ): Promise<PaginationRes<UserModel>> {
         const user = await this.usersRepository.findOne(id)
         if (!user) {
             throw new ConflictException()
@@ -24,7 +28,7 @@ export class UserCoachesListService {
             throw new ForbiddenException()
         }
 
-        const coaches = await this.usersRepository.find('role', UserRole.COACH)
+        const coaches = await this.usersRepository.find('role', UserRole.COACH, search)
 
         return this.paginationService.execute<UserModel>(coaches, pagination)
     }
