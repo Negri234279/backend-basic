c990c533216560099332cfa9b38e9508c8a6b6c9
negri234279 <rafalejo@live.com>
2023-06-06 00:12:42 +0200

    🔊 Add logger for http request

--- src/Core/app.module.ts
+++ src/Core/app.module.ts
@@ -1,14 +1,14 @@
-import { CommentsModule } from 'src/Contexts/Comments/comments.module'
-import { WorkoutsModule } from 'src/Contexts/Workouts/workouts.module'
-
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
+import { CommentsModule } from 'src/Contexts/Comments/comments.module'
+import { WorkoutsModule } from 'src/Contexts/Workouts/workouts.module'
 
 import { UsersModule } from '../Contexts/Users/users.module'
 import { AppController } from './infrastructure/app.controller'
 import configuration, { EnvVar } from './infrastructure/config/configuration'
 import { JwtAuthGuard } from './infrastructure/guards/jwtAuth.guard'
+import { LoggingMiddleware } from './infrastructure/middleware/logger.middleware'
 import { MongoModule } from './infrastructure/mongodb/mongo.module'
 
 @Module({
@@ -35,5 +35,9 @@ export class AppModule {
 
     constructor(private readonly configService: ConfigService<EnvVar>) {
         AppModule.port = this.configService.get<number>('PORT')
+    }
+
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggingMiddleware).forRoutes('*')
     }
 }
